b39012dd1ff8aa26a92683773d39dc6904b00694
Sergio Sebastian burgos <sebamayorde18@gmail.com>
2022-10-01 12:32:45 -0300

    casi terminado

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,63 @@
+import { useState, useEffect } from 'react'
+import styled from '@emotion/styled'
+import useSelectMonedas from '../hooks/useSelectMonedas'
+import { monedas } from '../data/monedas'
+
+const InputSubmit = styled.input `
+    background-color: #9497ff;
+    border: none;
+    width: 100%;
+    padding: 10px;
+    color: #FFF;
+    font-weight: 700;
+    text-transform: uppercase;
+    font-size: 20px;
+    border-radius: 5px;
+    margin-top: 30px;
+    transition: background-color .5s ease-in-out;
+    &:hover {
+        background-color: #787cfe;
+        cursor: pointer;
+    }
+`
+
+const Formulario = () => {
+
+const [ criptos, setCriptos] = useState([])
+const [ moneda, SelectMonedas ] = useSelectMonedas('Elige tu moneda', monedas)
+
+useEffect (() => {
+  const consultarAPI = async () => {
+    const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD'
+    const respuesta = await fetch(url)
+    const resultado = await respuesta.json()
+
+    const arrayCriptos = resultado.Data.map(cripto => {
+      const objeto = {
+        id:cripto.CoinInfo.Name,
+        nombre: cripto.CoinInfo.FullName
+      }
+      return objeto
+    })
+    setCriptos(arrayCriptos)
+  }
+  consultarAPI();
+}, [])
+
+
+  return (
+    <form>
+
+        <SelectMonedas />
+        {moneda}
+        <InputSubmit 
+        type="submit" 
+        value='Cotizar' 
+        
+        />
+    </form>
+
+  )
+}
+
+export default Formulario
---
+++ src/data/monedas.js
@@ -0,0 +1,13 @@
+
+
+const monedas = [
+    {id: 'USD', nombre: 'Dolar de Estados Unidos'},
+    {id: 'ARG', nombre: 'Pesos Argentinos'},
+    {id: 'EUR', nombre: 'Euro'},
+    {id: 'GBP', nombre: 'Libra Esterlina'},
+    {id: 'MXN', nombre: 'Pesos Mexicano'},
+  ]
+
+export {
+    monedas
+}
